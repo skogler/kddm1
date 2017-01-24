1aae6d344542c371ad0898fc8f77edd5cd64beb7
Samuel Kogler <samuel.kogler@gmail.com>
2017-01-24 22:28:35 +0100

    More clustering

--- clustering.py
+++ clustering.py
@@ -22,41 +22,52 @@ URL_KEY = 'url'
 ING_KEY = 'ingredients'
 TITLE_KEY = 'title'
 
-def clustering(X):
-    with open('cache/features.dat', 'rb') as fdat:
-        features = pickle.load(fdat)
+def clustering(X, feature_labels):
 
-    print(X.shape)
+    print('input dimensions:', X.shape)
     # X = StandardScaler().fit_transform(X)
-
-    clusterer = DBSCAN(eps=0.4, metric='cosine', algorithm='brute')
+    clusterer = DBSCAN(eps=1.0/4.0 , metric='jaccard')
 
     result = clusterer.fit(X)
 
-    print('fitting finished')
     core_samples_mask = np.zeros_like(result.labels_, dtype=bool)
     core_samples_mask[result.core_sample_indices_] = True
     labels = result.labels_
     
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Silhouette Coefficient: %0.3f'
-          % metrics.silhouette_score(X, labels))
-    print('Entropy of labels: {}'.format(metrics.cluster.entropy(labels)))
-
     clusters = set(labels)
+    n_clusters_ = len(clusters)
+
+    if -1 in labels:
+        n_clusters_ -= 1
+    
+    print('num clusters: {}'.format(n_clusters_))
+    print('silhouette: {:0.3f}'.format(metrics.silhouette_score(X, labels)))
+    print('entropy of labels: {:0.3f}'.format(metrics.cluster.entropy(labels)))
 
     num_samples_per_cluster = np.bincount(labels + 1)
 
-    plt.figure()
-    plt.bar(sorted(list(clusters)), num_samples_per_cluster)
-    plt.show()
+    cluster_centers = np.zeros(n_clusters_ * X.shape[1]).reshape(n_clusters_, X.shape[1])
 
+    for i in range(n_clusters_):
+        cluster_centers[i,:] = np.average(X[np.where(labels == i),:], 1)
+
+    plt.figure()
+    bars = plt.bar(sorted(list(clusters)), num_samples_per_cluster)
+    bars[0].set_color('r')
+    plt.title('Clustering of recipes')
+    plt.savefig('cluster_bars.png')
+
+    max_labellen = max([len(x) for x in feature_labels])
+
+    for i in range(n_clusters_):
+        print('Cluster {} (size {}):'.format(i, num_samples_per_cluster[i+1]))
+        for f in range(X.shape[1]):
+            print('  {:{width}} : {:0.4f}'.format(feature_labels[f], cluster_centers[i,f], width=max_labellen))
 
 if __name__ == '__main__':
     filename = 'cache/recipes_X.npz'
     with np.load(filename) as data:
-        X = data['X'][:1000,:]
-    clustering(X)
+        X = data['X'][:10000,:]
+    with open('cache/features.dat', 'rb') as fdat:
+        features = pickle.load(fdat)
+    clustering(X, features)
--- featureengineering.py
+++ featureengineering.py
@@ -3,6 +3,7 @@
 
 import sys
 import json
+import math
 import matplotlib.pyplot as plt
 import collections
 import numpy as np
@@ -25,7 +26,7 @@ def apply_threshold_to_counter(counter, threshold):
         if count < threshold:
             del counter_thresh[key]
 
-    print('threshold:', threshold, 'different ingredients:', len(counter_thresh.keys()))
+    print('threshold > {} recipes results in {} features'.format(threshold, len(counter_thresh.keys())))
 
     return counter_thresh
 
@@ -36,38 +37,106 @@ def load_recipes():
     with open('data/results_preprocessed.jl') as file:
         for row in file:
             recipe = json.loads(row)
-            ingredients = recipe[ING_KEY]
+            ingredients = list(set(recipe[ING_KEY]))
             recipes.append({TITLE_KEY: recipe[TITLE_KEY], ING_KEY: ingredients})
             ingredient_list.extend(ingredients)
 
     return recipes, ingredient_list
 
+def joint_probability(x, y, cooccurrence, num_recipes):
+    res = cooccurrence[x,y] / num_recipes
+    assert res >= 0 and res <= 1
+    return res
 
-def feature_engineering():
-    # analyze information
+def conditional_probability(y, x, cooccurrence):
+    res = cooccurrence[x,y] / cooccurrence[x,x]
+    assert res >= 0 and res <= 1
+    return res
 
-    recipes, ingredient_list = load_recipes()
+def conditional_entropy(y, X, cooccurrence, num_recipes):
+    ent = 0
+    for x in X:
+        cond = conditional_probability(y, x, cooccurrence)
+        joint = joint_probability(x, y, cooccurrence, num_recipes)
+        ent -=  joint * math.log2(cond)
+    assert ent >= 0
+    return ent
+
+def entropy(x, cooccurrence, num_recipes):
+    res = cooccurrence[x,x] / num_recipes
+    res = -res * math.log2(res)
+    assert res >= 0
+    return res 
+
+def relative_information_gain(y, X, cooccurrence, num_recipes):
+    enty = entropy(y, cooccurrence, num_recipes)
+    res = (enty - conditional_entropy(y, X, cooccurrence, num_recipes)) / enty
+    return res
+
+def feature_selection(recipes, ingredient_list):
 
     counter = Counter(ingredient_list)
-    print("different ingredients without threshold of recipes it appears in:", len(counter.keys()))
-    print('10 most common: ', counter.most_common(10))
-    print('now, apply threshold for minimum recipes the ingredients have to occur in')
-    apply_threshold_to_counter(counter, 20)
-    apply_threshold_to_counter(counter, 50)
-    apply_threshold_to_counter(counter, 100)
-    used = apply_threshold_to_counter(counter, 1000)
 
-    features = sorted(used.keys())
+    print("num ingredients:", len(counter.keys()))
+    print("num recipes:", len(recipes))
 
-    val = [1 if f in recipe[ING_KEY] else 0 
-            for recipe in recipes for f in features ]
+    # APPLY THRESHOLD
+    num_recipes = len(recipes)
+    low_threshold = 0.05
+    used = apply_threshold_to_counter(counter,  int(low_threshold * num_recipes))
 
-    X = np.array(val).reshape(len(recipes), len(features))
+    # BUILD CO-OCCURENCE MATRIX
+    ingredients_per_recipe = [r[ING_KEY] for r in recipes]
+    features = [(i, label, count) for i,(label, count) in enumerate(sorted(used.items(), key=lambda i: i[1], reverse=True))]
+    num_features = len(features)
 
+    X = np.array([1 if f in r else 0 
+            for r in ingredients_per_recipe for _,f,_ in features]).reshape(len(recipes), len(features))
+    cooccurrence = np.dot(np.transpose(X), X)
+
+    # CALCULATE ENTROPY
+    probabilities = [(i,label, count / num_recipes) for i,label,count in features]
+    rig = [(i,label, -prob * math.log(prob, 2)) for i,label,prob in probabilities]
+    rig.sort(key=lambda i: i[2], reverse=True)
+
+    # print('entropy: ', rig)
+    # print('ent(s)', entropy(0, cooccurrence, num_recipes))
+    # print('ent(p)', entropy(1, cooccurrence, num_recipes))
+    # print('cond prob. (s|p)', conditional_probability(0, 1, cooccurrence))
+    # print('cond prob. (p|s)', conditional_probability(1, 0, cooccurrence))
+    # print('joint prob. (s,p)', joint_probability(0, 1, cooccurrence, num_recipes))
+    # print('cond. entr(s, p)', conditional_entropy(0, [1], cooccurrence, num_recipes))
+    # print('cond. entr(p, s)', conditional_entropy(1, [0], cooccurrence, num_recipes))
+
+    # SELECT FEATURES BASED ON RELATIVE INFORMATION GAIN
+
+    selection = []
+    selection.append(rig.pop(0)[0])
+
+    while len(rig) > 0:
+        rig = [(yi, ylabel, relative_information_gain(yi, selection, cooccurrence, num_recipes)) for yi, ylabel, y in rig] 
+        rig.sort(key=lambda i: i[2], reverse=True)
+        if rig[0][2] <= 0:
+            break
+        selection.append(rig.pop(0)[0])
+
+    X = X[:, selection]
+    feature_labels = [label for i, label,_ in features if i in selection]
+
+    print(X.shape)
+    print(feature_labels)
+
+    return X, feature_labels
+
+
+def feature_engineering():
+    recipes, ingredient_list = load_recipes()
+
+    X, feature_labels = feature_selection(recipes, ingredient_list)
 
     np.savez('cache/recipes_X', X=X)
     with open('cache/features.dat', 'wb') as fdat:
-        pickle.dump(features, fdat)
+        pickle.dump(feature_labels, fdat)
 
 if __name__ == '__main__':
     feature_engineering()
